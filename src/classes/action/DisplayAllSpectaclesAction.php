029ff5e4055a67d31d46494bfc8790456180cb1f
Benj <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-11-12 10:29:57 +0100

    Ajout d'un nav avec les boutons pour trier + modif pour afficher que les spectacles

--- src/classes/action/DisplayAllSpectaclesAction.php
+++ src/classes/action/DisplayAllSpectaclesAction.php
@@ -15,13 +15,33 @@ use nrv\nancy\repository\NrvRepository;
 class DisplayAllSpectaclesAction extends Action{
     public function execute(): string
     {
-        $string ="";
+        $string ='<nav class ="filtre" >
+                <ul class ="buttonfiltre">
+                <li><a href="index.php?action=program&trier=date"><button >Lieu</button></a></li>
+                <li><a href="index.php?action=program&trier=lieu"><button >Date</button></a></li>
+                <li><button>Genre</button></li>
+                </ul>
+            </nav>';
         if($this->http_method === "GET"){
-
            $bd =NrvRepository::getInstance();
-           $resSoiree = $bd->getAllSoiree();
-           foreach ($resSoiree as $soiree){
-               $string .= RendererFactory::getRenderer($soiree)->render();
+           $resSpectacle = $bd->getAllSpectacle();
+//           if(isset($_GET['trier'])){
+//               $criteres = $_GET['trier'];
+//               switch ($criteres){
+//                   case "date":
+//                       usort($resSoiree, function ($a, $b) {
+//                           return strtotime($a->date) - strtotime($b->date);
+//                       });
+//                       break;
+//                   case "lieu":
+//                       usort($resSoiree, function ($a, $b) {
+//                           return strcmp($a->date, $b->thematique);
+//                       });
+//                       break;
+//               }
+//           }
+           foreach ($resSpectacle as $spectacle){
+               $string .= RendererFactory::getRenderer($spectacle)->render();
            }
         }
         return $string;
