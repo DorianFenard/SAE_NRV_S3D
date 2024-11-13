2a6424f45266566310de38fbf3799dd6a01b96f0
Benj <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-11-13 22:42:04 +0100

    Remise en forme pour que ce soit bcp plus compréhensible et plus simple d'utilisation pour la mise en place du html et du css

--- src/classes/action/DisplayAllSpectaclesAction.php
+++ src/classes/action/DisplayAllSpectaclesAction.php
@@ -13,13 +13,13 @@ class DisplayAllSpectaclesAction extends Action
         setlocale(LC_TIME, 'fr_FR.UTF-8', 'fr_FR', 'fr');
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['spectacle_id'])) {
-            $spectacleId = (int) $_POST['spectacle_id'];
+            $spectacleId = (int)$_POST['spectacle_id'];
 
             $favorites = isset($_COOKIE['favorites']) ? unserialize($_COOKIE['favorites']) : [];
 
             if (!in_array($spectacleId, $favorites, true)) {
                 $favorites[] = $spectacleId;
-                setcookie('favorites', serialize($favorites), time() + (60*60*24 * 30), "/");
+                setcookie('favorites', serialize($favorites), time() + (60 * 60 * 24 * 30), "/");
             }
         }
 
@@ -36,6 +36,19 @@ class DisplayAllSpectaclesAction extends Action
                 $genres[$spectacle->style] = $spectacle->style;
             }
         }
+
+        $listeLieuSpectacles = [];
+
+        foreach ($soirees as $soiree) {
+            $lieu = $soiree->lieu;
+            // Ajoute chaque couple "lieu-spectacle" à la liste
+            foreach ($soiree->spectacles as $spectacle) {
+                $listeLieuSpectacles[] = [
+                    'lieu' => $lieu,
+                    'spectacle' => $spectacle
+                ];
+            }
+        }
         $loginButton = isset($_SESSION['user'])
             ? '<a class="login-button" href="?action=logout">SE DÉCONNECTER</a>'
             : '<a class="login-button" href="?action=login">SE CONNECTER</a>';
@@ -47,9 +60,9 @@ class DisplayAllSpectaclesAction extends Action
                     </a> <div class="menu">
                         <a class="list-button" href="?action=">Acceuil</a>
                         <a class="list-button" href="?action=list">MA LISTE</a>
-                        <a class="program-button" href="?action=program">PROGRAMME</a>'.
-                         $adminButton.
-                        $loginButton .'              
+                        <a class="program-button" href="?action=program">PROGRAMME</a>' .
+            $adminButton .
+            $loginButton . '              
                     </div>
                     </header>
                     <div class="filters">';
@@ -58,15 +71,15 @@ class DisplayAllSpectaclesAction extends Action
    <button>Filtrer par date</button> <input type="checkbox" id="toggleButtonLieu" class="toggle-button"> <button>Filtrer par lieu</button> <input type="checkbox" id="toggleButtonGenre" class="toggle-button"> <button>Filtrer par genre</button>  <div class ="filtersDate"><ul>';
 
         foreach ($dates as $dateValue => $dateDisplay) {
-            $html .= '<li><a href="index.php?action=program&filter=date&value=' . urlencode($dateValue) . '">' . '<button class="filtersbutton">'. $dateValue .'</button>'. '</a></li>';
+            $html .= '<li><a href="index.php?action=program&filter=date&value=' . urlencode($dateValue) . '">' . '<button class="filtersbutton">' . $dateValue . '</button>' . '</a></li>';
         }
         $html .= '</ul></div><div class ="filtersLieu"><ul>';
         foreach ($lieux as $lieu) {
-            $html .= '<li><a href="index.php?action=program&filter=lieu&value=' . urlencode($lieu) . '">' .'<button class="filtersbutton">' . htmlspecialchars($lieu).'</button>' . '</a></li>';
+            $html .= '<li><a href="index.php?action=program&filter=lieu&value=' . urlencode($lieu) . '">' . '<button class="filtersbutton">' . htmlspecialchars($lieu) . '</button>' . '</a></li>';
         }
         $html .= '</ul></div><div class ="filtersGenre"><ul>';
         foreach ($genres as $genre) {
-            $html .= '<li><a href="index.php?action=program&filter=genre&value=' . urlencode($genre) . '">' .'<button class="filtersbutton">'. htmlspecialchars($genre).'</button>' . '</a></li>';
+            $html .= '<li><a href="index.php?action=program&filter=genre&value=' . urlencode($genre) . '">' . '<button class="filtersbutton">' . htmlspecialchars($genre) . '</button>' . '</a></li>';
         }
         $html .= '</ul></div></div>';
 
@@ -75,46 +88,73 @@ class DisplayAllSpectaclesAction extends Action
         $filterValue = $_GET['value'] ?? null;
 
         if ($filterType && $filterValue) {
-            $filteredSoirees = array_filter($soirees, function ($soiree) use ($filterType, $filterValue) {
+
                 switch ($filterType) {
                     case 'date':
-                        return $soiree->date === $filterValue;
+                        unset($listeLieuSpectacles);
+                        foreach ($soirees as $soiree) {
+                            $lieu = $soiree->lieu;
+                            $date = $soiree->date;
+                            if($date === $filterValue){
+                                // Ajoute chaque couple "lieu-spectacle" à la liste
+                                foreach ($soiree->spectacles as $spectacle) {
+                                    $listeLieuSpectacles[] = [
+                                        'lieu' => $lieu,
+                                        'spectacle' => $spectacle,
+                                        'date' => $date
+                                    ];
+
+                                }
+                            }
+                        }
+                        break;
                     case 'lieu':
-                        return stripos($soiree->lieu->nom, $filterValue) !== false;
+                        unset($listeLieuSpectacles);
+                        foreach ($soirees as $soiree) {
+                            $lieu = $soiree->lieu;
+                            $date = $soiree->date;
+                            if($lieu->nom===$filterValue){
+                                // Ajoute chaque couple "lieu-spectacle" à la liste
+                                foreach ($soiree->spectacles as $spectacle) {
+                                    $listeLieuSpectacles[] = [
+                                        'lieu' => $lieu,
+                                        'spectacle' => $spectacle,
+                                        'date' => $date
+                                    ];
+                                }
+                            }
+                        }
+                        break;
                     case 'genre':
-                        return array_reduce($soiree->spectacles, fn($carry, $spectacle) => $carry || stripos($spectacle->style, $filterValue) !== false, false);
+                        unset($listeLieuSpectacles);
+                        foreach ($soirees as $soiree) {
+                            $lieu = $soiree->lieu;
+                            $date = $soiree->date;
+                            // Ajoute chaque couple "lieu-spectacle" à la liste
+                            foreach ($soiree->spectacles as $spectacle) {
+                                if($spectacle->style == $filterValue){
+                                    $listeLieuSpectacles[] = [
+                                        'lieu' => $lieu,
+                                        'spectacle' => $spectacle,
+                                        'date' => $date
+                                    ];
+                                }
+
+                            }
+                        }
+                        break;
                     default:
-                        return true;
-                }
-            });
-        } else {
-            $filteredSoirees = $soirees;
+                        break;
+                };
         }
 
         $html .= '<div class="spectacles">';
-        $html .= implode('', array_map(function ($soiree) {
-            $dateFormatted = strftime('%A %d %B %Y', strtotime($soiree->date));
 
-            $dateRenderer = "<div class ='UnSpectacle'><h3>Date : <a href='index.php?action=program&filter=date&value=" . urlencode($soiree->date) . "'>$dateFormatted</a></h3>";
-            $lieuRenderer = RendererFactory::getRenderer($soiree->lieu)->render();
-            $spectaclesRenderer = implode('', array_map(function ($spectacle) {
-                $favorites = unserialize($_COOKIE['favorites'] ?? 'a:0:{}');
-                $isFavorite = in_array($spectacle->id, $favorites ?? [], true);
-                $favoriteButton = $isFavorite ? '<p>Déjà en favoris</p>' :
-                    '<form method="POST" action="">
-                        <input type="hidden" name="spectacle_id" value="' . htmlspecialchars((string)$spectacle->id) . '">
-                        <button type="submit">Ajouter aux favoris</button>
-                    </form> ';
-
-                $genreLink = "<a href='index.php?action=program&filter=genre&value=" . urlencode($spectacle->style) . "'>" . htmlspecialchars($spectacle->style) . "</a>";
-                $affichersoiree = "<a href='index.php?action=soiree&idspectacle=$spectacle->id' >Afficher les autres spectacles de la meme soirée</a>";
-
-
-                return RendererFactory::getRenderer($spectacle)->render() . "<p>Genre : $genreLink</p>" . $favoriteButton . $affichersoiree ."</div>"  ;
-            }, $soiree->spectacles));
-
-            return $dateRenderer . $lieuRenderer . $spectaclesRenderer;
-        }, $filteredSoirees));
+        foreach ($listeLieuSpectacles as $spectacle){
+            $html.='<h2> Date : '.strftime("%A %d %B %Y",strtotime($spectacle['date'])) .'</h2>';
+            $html.=RendererFactory::getRenderer($spectacle['lieu'])->render();
+            $html.=RendererFactory::getRenderer($spectacle['spectacle'])->render();
+        }
         $html .= '</div>';
 
 
